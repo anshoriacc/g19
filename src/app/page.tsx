acf744b9c0ddfc1f8925ab323e2f20056da4871a
anshori <anshoriacc@gmail.com>
2024-01-10 00:19:43 +0700

    update: restructur folder

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
-import About from "@/components/pages/home/about";
-import Hero from "@/components/pages/home/hero";
+import About from "@/components/home/about";
+import Hero from "@/components/home/hero";
 
 export default function Home() {
   return (
