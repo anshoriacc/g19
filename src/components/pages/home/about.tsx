067a59e53fee15eb81525d6d24e29c97c93c79b4
anshori <anshoriacc@gmail.com>
2024-01-09 23:56:13 +0700

    update: services card

--- src/components/pages/home/about.tsx
+++ src/components/pages/home/about.tsx
@@ -2,10 +2,22 @@
 
 import { Kanit } from "next/font/google";
 import { motion } from "framer-motion";
+import Image from "next/image";
 
 import { Button } from "@/components/ui/button";
 import { cn } from "@/utils";
 import ServicesCard from "./services-card";
+
+import mapIllustration from "@/assets/images/map-illustration.svg";
+import beachIllustration from "@/assets/images/beach-illustration.svg";
+import ticketIllustration from "@/assets/images/ticket-illustration.svg";
+import planeIllustration from "@/assets/images/plane-illustration.svg";
+import yellowCarIllustration from "@/assets/images/yellow-car-illustration.svg";
+import whiteCarIllustration from "@/assets/images/white-car-illustration.svg";
+import suitcaseIllustration from "@/assets/images/suitcase-illustration.svg";
+import backpackIllustration from "@/assets/images/backpack-illustration.svg";
+import walletIllustration from "@/assets/images/wallet-illustration.svg";
+import compassIllustration from "@/assets/images/compass-illustration.svg";
 
 const kanit = Kanit({ weight: ["700"], subsets: ["latin"] });
 
@@ -29,7 +41,7 @@ export default function About() {
         G19 Tour & Travel
       </h1>
 
-      <div className="flex flex-1 flex-col gap-4">
+      <div className="flex flex-1 flex-col items-center gap-4">
         <p className="text-center text-neutral-500">
           G19 Tour & Travel menawarkan pengalaman rental kendaraan dan
           perjalanan wisata yang tak terlupakan. Dengan armada kendaraan
@@ -37,15 +49,125 @@ export default function About() {
           kebutuhan perjalanan anda.
         </p>
 
-        <Button className="self-center">Hubungi Kami</Button>
+        <Button>Hubungi Kami</Button>
       </div>
 
       <div className="flex flex-wrap justify-center gap-6">
-        <ServicesCard type="rental" />
+        <ServicesCard type="rental">
+          <Image
+            src={mapIllustration}
+            alt="map illustration"
+            priority
+            className={cn(
+              "absolute -top-20 right-4 z-[1] rotate-12 scale-[1.5] grayscale-[0.9] transition-all duration-500",
+              "group-hover:-top-24 group-hover:right-4 group-hover:rotate-6 group-hover:grayscale-[0.4]",
+            )}
+          />
 
-        <ServicesCard type="tour" />
+          <Image
+            src={whiteCarIllustration}
+            alt="white car illustration"
+            priority
+            className={cn(
+              "absolute -bottom-2 right-24 z-[1] grayscale-[0.9] transition-all duration-1000",
+              "group-hover:-bottom-2 group-hover:right-64 group-hover:scale-[1.3] group-hover:grayscale-[0.4]",
+            )}
+          />
 
-        <ServicesCard type="carter" />
+          <Image
+            src={compassIllustration}
+            alt="compass illustration"
+            priority
+            className={cn(
+              "absolute -bottom-2 -right-2 z-[1] scale-[1.5] grayscale-[0.9] transition-all duration-500",
+              "group-hover:bottom-4 group-hover:right-4 group-hover:-rotate-6 group-hover:scale-[2] group-hover:grayscale-[0.4]",
+            )}
+          />
+        </ServicesCard>
+
+        <ServicesCard type="tour">
+          <Image
+            src={ticketIllustration}
+            alt="ticket illustration"
+            priority
+            className={cn(
+              "absolute -top-8 left-[45%] z-[2] scale-[1.2] grayscale-[0.9] transition-all duration-500",
+              "group-hover:-top-6 group-hover:right-4 group-hover:rotate-3 group-hover:scale-[1.5] group-hover:grayscale-[0.4]",
+            )}
+          />
+
+          <Image
+            src={mapIllustration}
+            alt="map illustration"
+            priority
+            className={cn(
+              "absolute -bottom-8 left-0 z-[1] rotate-12 grayscale-[0.9] transition-all duration-500",
+              "group-hover:-bottom-2 group-hover:left-4 group-hover:rotate-6 group-hover:scale-125 group-hover:grayscale-[0.4]",
+            )}
+          />
+
+          <Image
+            src={suitcaseIllustration}
+            alt="suitcase illustration"
+            priority
+            className={cn(
+              "absolute -bottom-12 left-[40%] z-[2] scale-[1] grayscale-[0.9] transition-all duration-500",
+              "group-hover:-bottom-8 group-hover:left-[30%] group-hover:-rotate-3 group-hover:scale-[1] group-hover:grayscale-[0.4]",
+            )}
+          />
+
+          <Image
+            src={beachIllustration}
+            alt="beach illustration"
+            priority
+            className={cn(
+              "absolute -right-2 bottom-0 z-[1] -rotate-12 scale-[1.25] grayscale-[0.9] transition-all duration-500",
+              "group-hover:bottom-0 group-hover:right-4 group-hover:-rotate-6 group-hover:scale-[1.6] group-hover:grayscale-[0.4]",
+            )}
+          />
+        </ServicesCard>
+
+        <ServicesCard type="carter">
+          <Image
+            src={planeIllustration}
+            alt="plane illustration"
+            priority
+            className={cn(
+              "absolute -right-4 top-2 z-[1] rotate-[15deg] grayscale-[0.9] transition-all duration-1000",
+              "group-hover:-top-6 group-hover:right-4 group-hover:scale-[1.25] group-hover:grayscale-[0.4]",
+            )}
+          />
+
+          <Image
+            src={yellowCarIllustration}
+            alt="yellow car illustration"
+            priority
+            className={cn(
+              "absolute -bottom-2 left-20 z-[1] scale-x-[-1] grayscale-[0.9] transition-all duration-1000",
+              "group-hover:-bottom-2 group-hover:left-40 group-hover:grayscale-[0.4]",
+            )}
+          />
+
+          <Image
+            src={backpackIllustration}
+            alt="backpack illustration"
+            priority
+            className={cn(
+              "absolute -bottom-2 left-4 z-[1] grayscale-[0.9] transition-all duration-500",
+              "group-hover:left-10 group-hover:rotate-12 group-hover:scale-[1.7] group-hover:grayscale-[0.4]",
+            )}
+          />
+
+          <Image
+            src={walletIllustration}
+            alt="wallet illustration"
+            priority
+            className={cn(
+              "absolute -bottom-2 left-6 z-[1] grayscale-[0.9] transition-all duration-500",
+              "group-hover:bottom-2 group-hover:left-16 group-hover:-rotate-6 group-hover:scale-[1.3] group-hover:grayscale-[0.4]",
+            )}
+          />
+        </ServicesCard>
       </div>
     </motion.div>
   );
--- src/components/pages/home/services-card.tsx
+++ src/components/pages/home/services-card.tsx
@@ -1,5 +1,9 @@
-import { cn } from "@/utils";
+import { PropsWithChildren } from "react";
 import Link from "next/link";
+import Image from "next/image";
+
+import { cn } from "@/utils";
+import patternFull from "@/assets/images/pattern-full.svg";
 
 const services = {
   rental: {
@@ -19,28 +23,39 @@ const services = {
   },
 };
 
-type Props = { type: keyof typeof services };
+type Props = PropsWithChildren<{ type: keyof typeof services }>;
 
-const ServicesCard = ({ type }: Props) => {
+const ServicesCard = ({ type, children }: Props) => {
   return (
     <Link
       href={`/${type}`}
       passHref
       className={cn(
-        "aspect-[4/3] w-[368px] rounded-2xl border border-neutral-200 bg-white hover:border-2 hover:border-primary dark:border-neutral-800 dark:bg-neutral-950",
-        "group relative overflow-hidden",
+        "aspect-[4/3] w-[368px] rounded-2xl border border-neutral-200 bg-white dark:border-neutral-800 dark:bg-neutral-950",
+        "group relative box-border overflow-hidden",
       )}>
       <h3
         className={cn(
-          "bg-gradient-to-b from-neutral-900 from-60% bg-clip-text text-3xl font-bold text-transparent group-hover:text-2xl dark:from-neutral-50",
-          "absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 gap-2 transition-all duration-500 group-hover:left-8 group-hover:top-8 group-hover:translate-x-0 group-hover:translate-y-0",
+          "bg-gradient-to-b from-neutral-900 from-60% bg-clip-text text-3xl font-bold text-transparent dark:from-neutral-50",
+          "absolute left-1/2 top-1/2 z-10 -translate-x-1/2 -translate-y-1/2 gap-2 transition-all duration-500 group-hover:left-8 group-hover:top-8 group-hover:translate-x-0 group-hover:translate-y-0",
         )}>
         {services[type].title}
       </h3>
 
-      <p className="absolute left-8 top-16 w-[304px] translate-x-10 translate-y-10 text-neutral-500 opacity-0 transition-all delay-200 duration-200 group-hover:visible group-hover:top-16 group-hover:translate-x-0 group-hover:translate-y-0 group-hover:opacity-100">
+      <p className="absolute left-8 top-16 z-10 w-[304px] translate-x-10 translate-y-10 text-neutral-500 opacity-0 transition-all delay-200 duration-200 group-hover:visible group-hover:top-16 group-hover:translate-x-0 group-hover:translate-y-0 group-hover:opacity-100">
         {services[type].description}
       </p>
+
+      {children}
+
+      <Image
+        src={patternFull}
+        alt="pattern"
+        priority
+        width={480}
+        height={480}
+        className="absolute left-1/2 top-1/2 z-[0] aspect-square -translate-x-1/2 -translate-y-1/2 -rotate-6 scale-[1.2] opacity-5 grayscale transition-all duration-500 group-hover:-rotate-12 group-hover:scale-[2] dark:invert"
+      />
     </Link>
   );
 };
