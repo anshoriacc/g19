a4c7d20dc4d05adabefd3471938cf1d4cd8c4928
anshori <anshoriacc@gmail.com>
2024-01-10 20:42:47 +0700

    update: login form

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,29 +1,10 @@
 import LoginForm from "@/components/auth/login-form";
 import Container from "@/components/layout/container";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
 
 export default function LoginPage() {
   return (
     <Container className="flex items-center justify-center">
-      <Card className="w-full max-w-[400px] rounded-2xl">
-        <CardHeader>
-          <CardTitle>Masuk</CardTitle>
-
-          <CardDescription>
-            Untuk mendapatkan akses sebagai administrator.
-          </CardDescription>
-        </CardHeader>
-
-        <CardContent>
-          <LoginForm />
-        </CardContent>
-      </Card>
+      <LoginForm />
     </Container>
   );
 }
--- src/components/auth/login-form.tsx
+++ src/components/auth/login-form.tsx
@@ -1,11 +1,14 @@
 "use client";
 
+import { useCallback } from "react";
 import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import * as z from "zod";
+import { motion } from "framer-motion";
 
 import {
   Form,
   FormControl,
-  FormDescription,
   FormField,
   FormItem,
   FormLabel,
@@ -13,56 +16,98 @@ import {
 } from "../ui/form";
 import { Button } from "../ui/button";
 import { Input } from "../ui/input";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "../ui/card";
+
+const formSchema = z.object({
+  username: z.string().min(4, {
+    message: "Username minimal 4 karakter.",
+  }),
+  password: z.string().min(8, { message: "Password minimal 8 karakter." }),
+});
 
 const LoginForm = () => {
-  const form = useForm();
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+  });
+
+  const onSubmit = useCallback((values: z.infer<typeof formSchema>) => {
+    console.log(values);
+  }, []);
 
   return (
-    <Form {...form}>
-      <form onSubmit={form.handleSubmit(() => {})} className="space-y-8">
-        <FormField
-          control={form.control}
-          name="username"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Username</FormLabel>
+    <motion.div
+      initial={{ opacity: 0, y: 40 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{
+        duration: 0.2,
+        type: "spring",
+        bounce: 50,
+        stiffness: 100,
+      }}>
+      <Card className="w-full max-w-[400px] rounded-2xl">
+        <CardHeader>
+          <CardTitle>Masuk</CardTitle>
 
-              <FormControl>
-                <Input placeholder="Input username" {...field} />
-              </FormControl>
+          <CardDescription>
+            Untuk mendapatkan akses sebagai administrator.
+          </CardDescription>
+        </CardHeader>
 
-              <FormMessage />
-            </FormItem>
-          )}
-        />
+        <CardContent>
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+              <FormField
+                control={form.control}
+                name="username"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Username</FormLabel>
 
-        <FormField
-          control={form.control}
-          name="password"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Password</FormLabel>
+                    <FormControl>
+                      <Input placeholder="Input username" {...field} />
+                    </FormControl>
 
-              <FormControl>
-                <Input
-                  placeholder="Input password"
-                  {...field}
-                  type="password"
-                />
-              </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
 
-              <FormMessage />
-            </FormItem>
-          )}
-        />
+              <FormField
+                control={form.control}
+                name="password"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Password</FormLabel>
 
-        <div className="flex">
-          <Button type="submit" className="ml-auto">
-            Masuk
-          </Button>
-        </div>
-      </form>
-    </Form>
+                    <FormControl>
+                      <Input
+                        placeholder="Input password"
+                        {...field}
+                        type="password"
+                      />
+                    </FormControl>
+
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+
+              <div className="flex">
+                <Button type="submit" className="ml-auto">
+                  Masuk
+                </Button>
+              </div>
+            </form>
+          </Form>
+        </CardContent>
+      </Card>
+    </motion.div>
   );
 };
 
