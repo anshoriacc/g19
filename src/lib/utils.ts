f227bb90897e5c29c388e1265deedbe0d08c9e6c
anshori <anshoriacc@gmail.com>
2024-01-31 10:04:06 +0700

    add media query hooks

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,4 +1,5 @@
 import { type ClassValue, clsx } from "clsx";
+import dayjs from "dayjs";
 import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
@@ -8,3 +9,17 @@ export function cn(...inputs: ClassValue[]) {
 export function delimiterFormatter(number: number) {
   return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
 }
+
+export const parseTime = (time: Date) => {
+  if (dayjs(time).isSame(dayjs(), "day")) {
+    return `Today ${dayjs(time).format("HH:mm")}`;
+  } else if (dayjs(time).isSame(dayjs().subtract(1, "day"), "day")) {
+    return `Yesterday ${dayjs(time).format("HH:mm")}`;
+  } else if (dayjs(time).diff(dayjs(), "day") > -7) {
+    return `${dayjs(time).format("ddd HH:mm")}`;
+  } else if (dayjs(time).isSame(dayjs(), "year")) {
+    return `${dayjs(time).format("DD MMM HH:mm")}`;
+  } else {
+    return `${dayjs(time).format("DD MMM YY HH:mm")}`;
+  }
+};
